bad8d8ccdd5afb03b04a5ed718733cc85659b7e3
littledivy <dj.srivastava23@gmail.com>
2021-02-06 15:26:54 +0530

    x

--- uc/main.rs
+++ uc/main.rs
@@ -1,30 +1,12 @@
-use std::env;
-use std::fs;
-use uwu::compiler::{Compiler, CompilerError};
-use uwu::parser::{ParseError, Parser};
-use uwu::tokenizer::Lexer;
-
 use codespan_reporting::diagnostic::{Diagnostic, Label};
 use codespan_reporting::files::SimpleFile;
 use codespan_reporting::term;
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
-
-enum Error {
-    ParseError(Vec<ParseError>),
-    CompilerError(CompilerError),
-}
-
-fn c(source: &str) -> Result<String, Error> {
-    let mut parser = Parser::new(Lexer::new(source));
-    let ast = parser.parse();
-    let errs = parser.get_errors();
-    // println!("{:#?}", ast);
-    if errs.len() > 0 {
-        return Err(Error::ParseError(errs));
-    }
-    let compiler = Compiler::new(ast);
-    Ok(compiler.compile().map_err(|c| Error::CompilerError(c))?)
-}
+use std::env;
+use std::fs;
+use swc_ecmascript::visit::Visit;
+use uwu::parser::parse;
+use uwu::scanner::Scanner;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -32,35 +14,8 @@ fn main() {
         let writer = StandardStream::stderr(ColorChoice::Always);
         let config = codespan_reporting::term::Config::default();
         let source = fs::read_to_string(&args[1]).expect("Unable to read file");
-        match c(&source) {
-            Ok(r) => println!("{}", r),
-            Err(e) => {
-                let file = SimpleFile::new(&args[1], source);
-                match e {
-                    Error::ParseError(e) => {
-                        for error in e {
-                            let diagnostic = Diagnostic::error()
-                                .with_message("Error")
-                                .with_labels(vec![Label::primary(
-                                    (),
-                                    error.current_token.loc..error.current_token.loc,
-                                )
-                                .with_message("parse error")])
-                                .with_notes(vec![error.msg]);
-
-                            term::emit(&mut writer.lock(), &config, &file, &diagnostic).unwrap();
-                        }
-                    }
-                    Error::CompilerError(e) => {
-                        let diagnostic =
-                            Diagnostic::error().with_message("Error").with_labels(vec![
-                                Label::primary((), e.loc..e.loc).with_message(format!("{}", e)),
-                            ]);
-
-                        term::emit(&mut writer.lock(), &config, &file, &diagnostic).unwrap();
-                    }
-                }
-            }
-        }
+        let mut scanner = Scanner {};
+        let ast = parse(&source);
+        scanner.visit_program(&ast, &ast);
     }
 }
