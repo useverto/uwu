d342463d687385f0b5f3693d122eb2c49eec9b6f
Divy Srivastava <dj.srivastava23@gmail.com>
2021-02-07 09:47:38 +0000

    feat!: decl scope scanner

--- crates/uwu-core/src/scanner.rs
+++ crates/uwu-core/src/scanner.rs
@@ -3,8 +3,12 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use swc_common::BytePos;
 use swc_ecmascript::{
-    ast::{CallExpr, Expr, ExprOrSuper, Ident, MemberExpr, Program},
-    visit::{noop_visit_type, Node, Visit},
+    ast::{
+        CallExpr, ClassExpr, Expr, ExprOrSuper, FnDecl, FnExpr, Function, Ident, MemberExpr, Pat,
+        Program, VarDecl,
+    },
+    utils::{find_ids, ident::IdentLike, Id},
+    visit::{noop_visit_type, Node, Visit, VisitWith},
 };
 
 #[derive(Debug, Clone)]
@@ -76,12 +80,65 @@ impl Visit for Scanner {
         let mut ctx = self.context.borrow_mut();
         let BytePos(lo) = ident.span.lo();
         let BytePos(hi) = ident.span.hi();
-        if !ctx.scope.has_global(ident.as_ref().to_string()) {
+        if !ctx.scope.contains(ident.as_ref().to_string()) {
             ctx.diagnostics.push(Diagnostic {
                 kind: ScannerErrorKind::ItemNotFound,
                 loc: (lo as usize, hi as usize),
                 msg: "Item not found in scope.".to_string(),
             });
+        }
+    }
+
+    // Adapted from https://github.com/denoland/deno_lint/blob/64232f8586a1c0c51a175ccd545344adf5d96def/src/scopes.rs#L159
+    fn visit_var_decl(&mut self, n: &VarDecl, _parent: &dyn Node) {
+        n.decls.iter().for_each(|v| {
+            v.init.visit_with(n, self);
+            let mut ctx = self.context.borrow_mut();
+            if let Some(expr) = &v.init {
+                if let Expr::Class(ClassExpr {
+                    ident: Some(class_name),
+                    ..
+                }) = &**expr
+                {
+                    if let Pat::Ident(var_name) = &v.name {
+                        if var_name.sym == class_name.sym {
+                            ctx.scope.vars.push(class_name.as_ref().to_string());
+                            return;
+                        }
+                    }
+                }
+            }
+
+            let ids: Vec<Id> = find_ids(&v.name);
+            for id in ids {
+                ctx.scope.vars.push(id.0.to_string());
+            }
+        })
+    }
+
+    fn visit_fn_decl(&mut self, n: &FnDecl, _parent: &dyn Node) {
+        let mut ctx = self.context.borrow_mut();
+        ctx.scope.functions.push(n.ident.as_ref().to_string());
+        drop(ctx);
+        n.function.visit_with(n, self);
+    }
+
+    fn visit_fn_expr(&mut self, n: &FnExpr, _: &dyn Node) {
+        let mut ctx = self.context.borrow_mut();
+        if let Some(ident) = &n.ident {
+            ctx.scope.functions.push(ident.as_ref().to_string());
+        }
+        drop(ctx);
+        n.function.visit_with(n, self);
+    }
+
+    fn visit_function(&mut self, n: &Function, _: &dyn Node) {
+        n.decorators.visit_with(n, self);
+        n.params.visit_with(n, self);
+
+        match &n.body {
+            Some(s) => s.stmts.visit_with(n, self),
+            None => {}
         }
     }
 }
