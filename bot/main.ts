b287977811e8447688fe07cae0b7bab349f7f930
Divy Srivastava <dj.srivastava23@gmail.com>
2021-02-12 11:16:53 +0000

    refactor: update lib and bot to pass in scope to scanner

--- bot/main.ts
+++ bot/main.ts
@@ -4,6 +4,9 @@ const BOT_TOKEN = await Deno.readTextFile("bot/token.txt");
 
 console.info("Starting Bot...");
 
+const GLOBALS: Array<String> = ["Math", "parseInt", "parseFloat"];
+const globalsInfo = GLOBALS.join('\n');
+
 start({
   token: BOT_TOKEN,
   intents: ["GUILDS", "GUILD_MESSAGES"],
@@ -12,13 +15,16 @@ start({
       console.log("Successfully connected to gateway");
     },
     messageCreate(message) {
+      if(message.content.startsWith("!uwu scope")) {
+          message.reply("The following GLOBALS were found: " + "```js\n" + globalsInfo + "```")
+      }
       if (message.content.startsWith("!uwu eval")) {
         let tokens = md.parse(message.content);
         for (let node in tokens) {
           let t = tokens[node];
           if (t.tag == "code") {
             try {
-              let result = scan(t.content);
+              let result = scan(t.content, GLOBALS);
               if(result === "true") {
                 // Hmmm
                 message.reply("```js\n" + JSON.stringify(eval(t.content)) + "```");
